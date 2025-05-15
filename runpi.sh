f68c5b8ee8db6943b7703b0708a9e5e10af44c5a
Tezzy <zacharyeverett.00@gmail.com>
2025-05-15 17:05:43 +1000

    runpi file

---
+++ runpi.sh
@@ -0,0 +1,11 @@
+git checkout pi
+git add .
+git commit -m "to pi"
+git push
+git checkout main
+ssh raspberrypi "
+    cd lol_champ_similarity_v2
+    git pull
+    python3 request.py
+"
+scp raspberrypi/lolchamp_similarity_v2/database.db database.db
